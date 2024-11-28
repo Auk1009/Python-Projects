fdbbf610a966396bc4487293e32a4baf8776d17a
Aryan U Kiran <aryanukiran@Administrators-MacBook-Pro.local>
2024-11-28 21:50:44 +0530

    understanding os

--- undersanding_libraries/os.py
+++ undersanding_libraries/os.py
@@ -1,5 +1,23 @@
 import os
 
-print('current working directory: ',os.getcwd())
-os.rmdir('hello.py')
-print('content of the current directory: ',os.listdir())
+
+def find_file(target_file,path='/Users/aryanukiran/python/Python-Projects'):
+    for root, files in os.walk(path):
+    
+        if target_file in files:
+            print(f'found {files}, in {os.path.join(root,target_file)}')
+
+def add_file(name_folder:str):
+    os.system(f'cd ..')
+    os.mkdir(name_folder)
+    os.system(f"cd {name_folder}")
+    with open('autofile.py', 'w') as f:
+        f.write('Hello this is a new file')
+
+
+
+add_file('hello23')
+
+    
+
+
